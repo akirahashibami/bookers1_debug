ec0bfa5af3e509c7b861cde0af3ed475359c603f
akirahashibami <hashibami2207@gmail.com>
2020-08-01 01:32:36 +0000

    [Complate] bookers1_debug

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,6 @@
+class HomesController < ApplicationController
+
+  def top
+    # code
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+
+  get 'top' => 'homes#top'
+  root 'homes#top'
+  resources :books
+end
